af5c021227fb0ffa643dab850598c48b8ef52bc8
Mikael Kvist <co17de11@gmail.com>
2025-02-04 17:07:40 +0100

    fix: launch script

--- tool/launch.sh
+++ tool/launch.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+
 echo "Runnning Todo application...";
-cd /home/kuzan/devs/todo/
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd )
 
-docker-compose up > /dev/null 2>&1 &
+main() (
+    docker-compose up > /dev/null 2>&1 &
 
-ready=$(curl -sI http://localhost:3000 | grep "200 OK" | wc -l)
-while [[ "$ready" != 1 ]]
-do 
-    sleep 0.1;   
     ready=$(curl -sI http://localhost:3000 | grep "200 OK" | wc -l)
-done
-google-chrome http://localhost:3000 & pid=$!
-#PIDS=$(ps ax | grep firefox | grep -v grep | awk '{print $1}')
-#for pid in $PIDS; do
-#    wait $pid;
-#done
-wait $pid;
+    while [[ "$ready" != 1 ]]
+    do 
+        sleep 0.1;   
+        ready=$(curl -sI http://localhost:3000 | grep "200 OK" | wc -l)
+    done
+    google-chrome http://localhost:3000 & pid=$!
+    #PIDS=$(ps ax | grep firefox | grep -v grep | awk '{print $1}')
+    #for pid in $PIDS; do
+    #    wait $pid;
+    #done
+    wait $pid;
 
-docker-compose down > /dev/null 2>&1
+    docker-compose down > /dev/null 2>&1
+)
+
+main "$@"
+
 echo "..application shut down";
